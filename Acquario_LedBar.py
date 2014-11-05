a7470756c002b8686f0001712a72bc6eb602f1e9
Xendorf <github@ganasa.net>
2014-11-05 14:31:56 +0100

    Ottimizzazione assegnazione colore iniziale led

--- Acquario_LedBar.py
+++ Acquario_LedBar.py
@@ -5,45 +5,27 @@ from time import sleep
 class Acquario_LedBar():
 
     # impostazioni iniziali dei led
-    _led = range(0,25)
+    _led = range(0,24)
 
     SPENTO = 0
     VERDE = 1
     ROSSO = 2
     GIALLO = 3
 
-
     def __init__(self):
         
         from Adafruit_LED_Backpack import BicolorBargraph24
 
-        #prima barra
-        self._led[0]  = VERDE
-        self._led[1]  = VERDE
-        self._led[2]  = VERDE
-        self._led[3]  = VERDE
-        self._led[4]  = GIALLO
-        self._led[5]  = GIALLO
-        self._led[6]  = GIALLO
-        self._led[7]  = GIALLO
-        self._led[8]  = ROSSO
-        self._led[9]  = ROSSO
-        self._led[10] = ROSSO
-        self._led[11] = ROSSO
-        # seconda barra
-        self._led[12] = VERDE
-        self._led[13] = VERDE
-        self._led[14] = VERDE
-        self._led[15] = VERDE
-        self._led[16] = GIALLO
-        self._led[17] = GIALLO
-        self._led[18] = GIALLO
-        self._led[19] = GIALLO
-        self._led[20] = ROSSO
-        self._led[21] = ROSSO
-        self._led[22] = ROSSO
-        self._led[23] = ROSSO
-
+        for i in range(0,4):
+            #prima barra
+            self._led[i] = VERDE
+            self._led[i+4] = GIALLO
+            self._led[i+8] = ROSSO
+            #seconda barra
+            self._led[i+12] = VERDE
+            self._led[i+16] = GIALLO
+            self._led[i+20] = ROSSO
+            
         self._BarGraph = BicolorBargraph24.BicolorBargraph24()
         self._BarGraph.begin()
 
